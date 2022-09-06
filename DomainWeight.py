6cdc41ac97aa51800e24a545c920902c25df3a7d
mlxwlX <108117635+mlxwlX@users.noreply.github.com>
2022-09-06 16:41:04 +0800

    Update DomainWeight.py

--- DomainWeight.py
+++ DomainWeight.py
@@ -1,7 +1,8 @@
 import argparse
 import textwrap
 import requests
-import re,random,os,time
+import time
+from bs4 import BeautifulSoup
 import prettytable as pt
 
 
@@ -11,26 +12,22 @@ def main(url):
     response = session.get(full_url)
     response_text = response.text
 
-    with open('response_text.txt','w',encoding='u8')as f:
-        f.write(response_text)
-    with open('response_text.txt', 'r', encoding='u8')as rd:
-        x = rd.readlines()
-    for i in x:
-        if '/images/br/' in i:
-            text = i.strip()
+    # bs4对html进行解析，寻找回复包中的关键字
+    bs = BeautifulSoup(response_text, "html.parser")
+    bs_list = []
+    for item in bs.find_all("img"):
+        bs_list.append(item.get("src"))
+    for num in range(0, 15):
+        logo = f"//statics.aizhan.com/images/br/{num}.png"
+        if logo in bs_list:
+            print(f"{url}的百度权重是{num}")
             break
-        elif '您的查询太频繁了' in i:
-            print('您的查询太频繁了')
-            return False
-    pattern = re.compile(r'\d+')
-    result = pattern.findall(text)
-    print(f"{url}的百度权重是{result[0]}")
 
     Domain.append(url)
-    Weight.append(result[0])
+    Weight.append(num)
+    # 做延时，避免访问频繁
+    time.sleep(1)
 
-    os.remove('response_text.txt')
-    time.sleep(2)
 
 def banner():
     banner = """ 
@@ -40,7 +37,7 @@ def banner():
    / / / _ \ '__/ _ \     | |  | '_ \| '__/ _ \/ _` | __|
   / /_|  __/ | | (_) |    | |  | | | | | |  __/ (_| | |_ 
  /_____\___|_|  \___/     |_|  |_| |_|_|  \___|\__,_|\__|
-                                                                                                                                                                                                                 
+
     """
     print(banner)
 
@@ -52,20 +49,25 @@ def result():
     table.add_column('百度权重', [i for i in Weight])
     print(table)
 
+
 if __name__ == '__main__':
     banner()
     Domain = []
     Weight = []
     parser = argparse.ArgumentParser(description='百度权重查询', formatter_class=argparse.RawDescriptionHelpFormatter,
                                      epilog=textwrap.dedent('''example:python DomainWeight.py -f url.txt'''))
-    parser.add_argument("-f", type=argparse.FileType(mode='r',encoding='u8'), required=True,dest='file',help="Input file directory")
-    args = parser.parse_args()
-    with args.file as file:
-        res = file.readlines()
-    for url in res:
-        url = url.strip()
-        if main(url) == False:
-            print('程序终止')
-            break
-    result()
+    parser.add_argument("-f", type=argparse.FileType(mode='r', encoding='u8'), required=False, dest='file',
+                          help="Input file directory")
 
+    parser.add_argument('-u', help='target url', dest='url',required=False)
+    args = parser.parse_args()
+
+    if args.url:
+        main(args.url)
+    if args.file:
+        with args.file as file:
+            res = file.readlines()
+        for url in res:
+            url = url.strip()
+            main(url)
+    result()
